6b5ccce20019f7681731f1a95e6de9fdf78b8141
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-01-15 00:11:19 +0800

    SUPPORT: dark theme and menu buttons

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
 	return (
 		<Container>
-			<Box borderRadius='lg' bg="red" p={3} align="center">
+			<Box borderRadius='lg' bg="red" p={3} mb={6} align="center">
 				Hello, I'm a full-stack developer based in Hong Kong!
 			</Box>
-			<Box display={{ md: 'flex' }}>
+			<Box display={{ md: 'flex' }} >
 				<Box flexGrow={1}>
 					<Heading as="h2" variant="page-title"> WOO Yu Kit Vincent</Heading>
 					<p>Mobile Appliction Developer, Team Leader, Project Manager</p>
