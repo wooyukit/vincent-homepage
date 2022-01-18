3ae8228a414fe044888a52c9a3f8b6139537a09f
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-01-18 17:06:01 +0800

    ADDED: 404 not found page

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import NextLink from 'next/link'
+import { Box, Heading, Container, Text, Divider, Button } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+
+const NotFound = () => {
+	return (
+		<Layout>
+			<Container>
+				<Heading as="h1">Not Found</Heading>
+				<Text>The page you are lokking for was not found.</Text>
+				<Divider my={6} />
+				<Box my={6} align="center" >
+					<NextLink href="/">
+						<Button colorScheme='teal'>Return to home</Button>
+					</NextLink>
+				</Box>
+			</Container>
+		</Layout>
+	)
+}
+
+export default NotFound
