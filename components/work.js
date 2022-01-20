089120488ddfb7b630ed868d23b0e53768d25ed5
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-01-20 15:00:20 +0800

    UPDATED: the back button style of portfolio

--- components/work.js
+++ components/work.js
@@ -1,15 +1,15 @@
 import NextLink from 'next/link'
-import { Heading, Box, Link, Badge, Center, HStack } from '@chakra-ui/react'
+import { Heading, Box, Link, Badge, Center, Tag } from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
 import Image from 'next/image'
 
 export const Title = ({ children }) => (
-	<Box>
+	<Box mt={2}>
 		<NextLink href="/portfolio">
-			<Link><HStack><ChevronLeftIcon fontSize={28} /><>Portfolio</></HStack></Link>
+			<Link><Tag colorScheme="teal" variant="outline" size="lg"><ChevronLeftIcon fontSize={24} />Portfolio</Tag></Link>
 		</NextLink>
 		<Box>
-			<Heading display="inline-block" as="h3" fontSize={20} my={6}>
+			<Heading display="inline-block" as="h3" fontSize={24} my={4}>
 				{children}
 			</Heading>
 		</Box>
