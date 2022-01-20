4792c8a642058d1f989fab4070080f7afd51e2e0
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-01-20 15:27:12 +0800

    UPDATED: the animation of page transition

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -3,12 +3,12 @@ import Head from 'next/head'
 import { GridItemStyle } from "../grid-item";
 
 const variants = {
-	hidden: { opacity: 0, x: 0, y: 20 },
-	enter: { opacity: 1, x: 0, y: 0 },
+	hidden: { opacity: 0, y: 20 },
+	enter: { opacity: 1, y: 0 }
 }
 
 const Layout = ({ children, title }) => (
-	<motion.article initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ duration: 0.4, type: 'easeInOut' }} style={{ position: 'relative' }}>
+	<motion.article initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ duration: 0.8, type: 'easeInOut' }}>
 		<>
 			{title && (
 				<Head>
