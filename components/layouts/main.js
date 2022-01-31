fb53fb660a8e6192664d475d37ea5dd56ce8668f
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2022-01-31 21:07:19 +0800

    UPDATED: removed the bottom padding of planet

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,7 +6,7 @@ import Planet from '../planet.js'
 
 const Main = ({ children, router }) => {
 	return (
-		<Box as="main" pb={8}>
+		<Box as="main">
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<title>Vincent Homepage</title>
